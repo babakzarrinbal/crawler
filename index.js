480900fee9c8b0b415ec87c18cf46f5f4f9a7d06
babak zarrinbal <babak.zarrinbal@gmail.com>
2019-03-08 13:06:37 +0330

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const f2m = require("./linkscrappers/film2movies");
+global.delay = timout => {
+  return new Promise(resolve => setTimeout(resolve, timout));
+};
+global.goto = (page, url, options = {}) => {
+  return new Promise(async mresolve => {
+    try {
+      if (options.stop || options.stopcallback) {
+        options.stop = options.stop || 3000;
+        page.goto(url, options);
+        if (options.stopcallback) {
+          while (!(await page.evaluate(options.stopcallback))) {
+            await delay(options.stop || 2000);
+          }
+        } else {
+          await delay(options.stop || 2000);
+        }
+        await page.evaluate(() => {
+          window.stop();
+        });
+        mresolve();
+      } else {
+        await page.goto(url, options);
+      }
+      mresolve();
+    } catch (err) {
+      console.log("babak------------------", err);
+    }
+  });
+};
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  try {
+    let f2mmovies = await f2m(browser, { pages: [1, 2] });
+    console.log("finished");
+    fs.writeFile(
+      "./test/imdbtest.json",
+      JSON.stringify(f2mmovies, null, 2),
+      () => {}
+    );
+  } catch (err) {
+    console.log(err);
+  }
+})();
---
+++ linkscrappers/film2movies.js
@@ -0,0 +1,109 @@
+const host = "https://www.film2movie.ws";
+const defaults = {
+  pages: 1,
+  queue: 8,
+  logging: true,
+};
+
+var async = require("async");
+var linkpage = [],
+  movies = [],
+  detailqueue,
+  dpages = [],
+  logging,
+  browser;
+
+/**
+ *
+ * @param {*} browser puppeteer browser object
+ * @param {object} options
+ * @param {number|number[]} options.pages page number to scrap or start to end array
+ * @param {number} options.queue count of parallel pages
+ * @param {boolean} options.logging enables console.log
+ */
+var main = function(brw, options = {}) {
+  logging = options.logging || defaults.logging;
+  pages = options.pages || defaults.pages;
+  queue = options.queue || defaults.queue;
+  options;
+  browser = brw;
+  if (!Array.isArray(pages)) pages = [pages];
+  return new Promise(async resolve => {
+    detailqueue = async.queue(getmovielinks, queue);
+    while (dpages.length < queue) {
+      let dp = {
+        id: dpages.length + 1,
+        page: null,
+        free: true,
+      };
+      dpages.push(dp);
+      dp.page = await browser.newPage();
+    }
+
+    await getpagelinks(pages);
+    detailqueue.drain = async () => {
+      await Promise.all(dpages.map(dp => dp.page.close()));
+      resolve(movies);
+    };
+  });
+};
+
+var getpagelinks = async function(pages) {
+  let page = await browser.newPage();
+  return new Promise(async resolve => {
+    let spage = pages[0] || 1;
+    let fpage = pages[1] || pages[0] || 1;
+    for (let i = spage; i <= fpage; i++) {
+      await goto(page, host + "/page/" + i, { stop: 5000 });
+      pagelinks = await page.$$eval(".main>.box>.content>a", el =>
+        el.map(e => e.href)
+      );
+      logging && console.log(`page ${i} pagelinks extracted!`);
+      pagelinks.map(async link => {
+        detailqueue.push(link);
+      });
+      linkpage = [...linkpage, ...pagelinks];
+    }
+    resolve();
+  });
+};
+
+var getmovielinks = async function(link, resolver = () => {}) {
+  let pageobj = dpages.find(dp => dp.free);
+  while (!pageobj) {
+    await delay(2000);
+    return detailqueue.push(link, resolver);
+  }
+
+  let page = pageobj.page;
+  let movie = {};
+  pageobj.free = false;
+  await goto(page, link, { stop: 7000 });
+
+  atags = movie.links = await page.$$eval("a", el => {
+    return el.map(e => ({ href: e.href, text: e.innerText }));
+  });
+  try {
+    movie.links = atags
+      .filter(
+        a =>
+          a.text == "لینک مستقیم" &&
+          !a.href.includes(host) &&
+          ["mp4", "mkv"].includes(a.href.slice(-3).toLowerCase())
+      )
+      .map(a => a.href);
+    movie.imdb = (
+      atags.find(a => a.href.includes("imdb.com")) || { href: "" }
+    ).href.toLowerCase();
+    imdbarr = movie.imdb.split("/");
+    movie.imdb = imdbarr[imdbarr.indexOf("title") + 1];
+    logging && console.log(`${movie.imdb} links extracted!`);
+  } catch (err) {
+    console.log(err);
+  }
+  pageobj.free = true;
+  if (movie.imdb) movies.push(movie);
+  resolver();
+};
+
+module.exports = main;
